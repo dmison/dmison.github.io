f348dbb7d0efee571d90f76ee56093ae8fbddd6e
Dana Mison <dmison@me.com>
2020-10-18 15:29:21 +1000

    theme

---
+++ themes/mango/scripts/fancybox.js
@@ -0,0 +1,28 @@
+var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+
+/**
+* Fancybox tag
+*
+* Syntax:
+*   {% fancybox /path/to/image [/path/to/thumbnail] [title] %}
+*/
+
+hexo.extend.tag.register('fancybox', function(args){
+  var original = args.shift(),
+    thumbnail = '';
+
+  if (args.length && rUrl.test(args[0])){
+    thumbnail = args.shift();
+  }
+  
+  title = args.shift();
+
+  var styles = args.join('');
+
+  return '<div class="fancy-image-box" style="' + styles + '"></a>' +
+    '<a class="fancybox" href="' + original + '" title="' + title + '">' +
+    '<img src="' + (thumbnail || original) + '" alt="' + title + '" />' +
+    '</a>' +
+    (title ? '<span class="caption">' + title + '</span>' : '') +
+    '</div>';
+});
